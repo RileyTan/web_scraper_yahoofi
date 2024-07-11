a897208d475a1b79294d862a731a5844b82dce7b
RileyTan <149601796+RileyTan@users.noreply.github.com>
2024-07-11 07:46:52 +0000

    Thu, Jul 11, 2024, 3:46 PM +08:00

--- webscraper.py
+++ webscraper.py
@@ -12,17 +12,12 @@ url_financials = 'https://sg.finance.yahoo.com/quote/{}/financials/'
 
 stock = 'DIS'
 
-response = requests.get(url_financials.format(stock))
+response = requests.get(url_financials.format(stock) , headers={'user-agent':'my-app'})
 soup = BeautifulSoup(response.text, 'lxml')
 
 # there are many scripts, use re to solve. also, compile() lets us reuse a pattern
-pattern = re.compile(r'/\* -- Data -- \*/[\s\S]*')
-scripts = soup.find_all('script')
+pattern = re.compile(r'\s--\sData\s--\s')
+script_data = soup.find('script', text=pattern).contents[0]
 
-for script in scripts:
-    if pattern.search(script.string):
-        script_data = script.string
-        print(script_data[:500])
-        break
-
+print(script_data)
 
