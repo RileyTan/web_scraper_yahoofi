25f30813878bdfd4e3a8c40ca49551e149e387a5
RileyTan <149601796+RileyTan@users.noreply.github.com>
2024-07-11 07:18:47 +0000

    Thu, Jul 11, 2024, 3:18 PM +08:00

--- webscraper.py
+++ webscraper.py
@@ -5,10 +5,12 @@ from io import StringIO
 from bs4 import BeautifulSoup
 import requests
 
+
 url_stats = 'https://sg.finance.yahoo.com/quote/{}/key-statistics/'
 url_profile = 'https://sg.finance.yahoo.com/quote/{}/profile/'
 url_financials = 'https://sg.finance.yahoo.com/quote/{}/financials/'
 
 stock = 'DIS'
 
-response = requests.get(url_financials
+response = requests.get(url_financials.format(stock))
+soup = BeautifulSoup(response.text, 'lxml')
