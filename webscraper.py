929042a6e0941f2e526ea8834a15cdcdf3de0ed1
RileyTan <149601796+RileyTan@users.noreply.github.com>
2024-07-11 07:07:08 +0000

    Thu, Jul 11, 2024, 3:07 PM +08:00

--- webscraper.py
+++ webscraper.py
@@ -5,4 +5,8 @@ from io import StringIO
 from bs4 import BeautifulSoup
 import requests
 
+url_stats = "https://sg.finance.yahoo.com/quote/DIS/key-statistics/"
+url_profile = "https://sg.finance.yahoo.com/quote/DIS/profile/"
+url_financials = "https://sg.finance.yahoo.com/quote/DIS/financials/"
+
 
