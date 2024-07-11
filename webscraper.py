6e6eedd4b9308220d8c5a4be8e2fa4e3e481efc1
RileyTan <149601796+RileyTan@users.noreply.github.com>
2024-07-11 07:38:47 +0000

    Thu, Jul 11, 2024, 3:38 PM +08:00

--- webscraper.py
+++ webscraper.py
@@ -17,6 +17,12 @@ soup = BeautifulSoup(response.text, 'lxml')
 
 # there are many scripts, use re to solve. also, compile() lets us reuse a pattern
 pattern = re.compile(r'/\* -- Data -- \*/')
-script_data = soup.find('script', text=pattern).contents[0]
+script_tag = soup.find('script', text=pattern)
+if script_tag is not None:
+    script_data = script_tag.contents[0]
+else:
+    print("No match found")
 
-print(script_data[:500])
+#script_data = soup.find('script', text=pattern).contents[0]
+
+#print(script_data[:500])
