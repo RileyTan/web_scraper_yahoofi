1b2bad6761ec9ef0e9797e3ae5d0a5d27142e7df
RileyTan <149601796+RileyTan@users.noreply.github.com>
2024-07-12 09:09:20 +0000

    Fri, Jul 12, 2024, 5:09 PM +08:00

--- webscraper2.py
+++ webscraper2.py
@@ -5,4 +5,6 @@ html = requests.get("https://sg.finance.yahoo.com/quote/DIS/key-statistics/")
 # fromstring() expects a string. html.content is the string
 doc = lxml.html.fromstring(html.text)
 
+# if making a web-based API, JSON as output is better choice, easily consumed by more prog languages
+# if planning to do data analysis/manipulation, CSV might be more suitable, can import into Excel
 
