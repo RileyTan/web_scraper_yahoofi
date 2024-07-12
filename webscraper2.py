b3655b9b87ac34b2d6e76012f868dfed92a0ee1c
RileyTan <149601796+RileyTan@users.noreply.github.com>
2024-07-12 09:02:38 +0000

    Fri, Jul 12, 2024, 5:02 PM +08:00

--- webscraper2.py
+++ webscraper2.py
@@ -3,4 +3,5 @@ import lxml.html
 
 html = requests.get("https://sg.finance.yahoo.com/quote/DIS/key-statistics/")
 # fromstring() expects a string. html.content is the string
-doc = lxml.html.fromstring(html.content)
+doc = lxml.html.fromstring(html.text)
+
